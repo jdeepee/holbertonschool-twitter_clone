e3cf8c69f6ed7cfab67ba5b99b671a5d6fd5df11
jdeepee <joshuadparkin@gmail.com>
2016-03-06 18:29:47 -0800

    Javascript done

---
+++ reply.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+	var reply = document.querySelectorAll('#button-reply'); // find all elements with reply id
+	var r = 0, r_length = reply.length; // create & set new vars to 0 and to number of reply btns (length)
+		for (r; r < r_length; r++) { // iterates through all reply btns adding event listeners to each
+		reply[r].addEventListener('click', function() { // when click is heard do the following:
+		var parent = this.closest('#post-status-button-div');  // set target 1 for toggling
+		var post = this.parentNode.nextElementSibling; // set target 2 for toggle
+		post.toggle(); 
+		parent.toggle();
+		});
+	}
+});
