68eb2ef94485b21b963e0fb23e3771330fdc74e5
Carole Bou <35@p4-5.hbtn.io>
2016-03-22 15:09:03 -0700

    Completed mandatory tasks incl reply button on all statuses

--- ajax.js
+++ ajax.js
@@ -11,8 +11,8 @@ function ajaxGet(url, onSuccess){ // function called ajaxGet that expects 2 argu
 	    if(xmlhttp.readyState == 4 && xmlhttp.status == 200)
 	    {
 	        onSuccess(xmlhttp.responseText); // onSuccess: a callback that will run after the call was successfully performed and that expects one argument; a string that contains what's in the file that was just fetched
-	    }
-	reply();     
+	    	reply(); // calling reply function again after extra statuss are loaded so that reply button works on them too
+	    }    
 	}
 	xmlhttp.open('GET', url, true); // url: a string that contains the local URL to be calling
 
