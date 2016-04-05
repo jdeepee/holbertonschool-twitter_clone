96d30872c97e7409235234aad1a54144b7334bf2
jdeepee <joshuadparkin@gmail.com>
2016-04-05 13:11:40 -0700

    Removed JS files

--- post_a_status.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() { 
-	document.getElementById("post-status-btn").addEventListener("click", postpre);
-	function postpre() { // call toggle function on poststatusbtn
-		var elem = document.getElementById('statuspost');
-		elem.toggle(); // call toggle on div containing new status post
-		var elem = document.getElementById('post-status-btn');
-		elem.toggle();
-	}
-});
--- toggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() { // wait til page is loaded
-	HTMLElement.prototype.toggle = function() { 
-	   if (this.classList.contains("hidden")) { // if element is hidden, remove hidden.
-	    this.classList.remove('hidden');
-	  } else {
-	    this.classList.add('hidden'); // else add hidden
-	  }
-	}
-});
